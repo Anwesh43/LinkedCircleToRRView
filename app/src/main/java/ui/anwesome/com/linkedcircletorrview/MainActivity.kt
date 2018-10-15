02f6c753f976ebd40e4246a6216521ad1254040d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-15 23:17:51 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/linkedcircletorrview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedcircletorrview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.linkedcircletorrview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circletorrview.LinkedCircleToRRView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedCircleToRRView.create(this)
     }
 }
