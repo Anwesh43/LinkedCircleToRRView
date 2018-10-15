7c43c1dc85f076e1ba789457000d4835ec8f61d1
Anwesh43 <anweshthecool0@gmail.com>
2018-10-15 22:49:37 +0530

    adding CRRNode class

--- linkedcircletorrview/src/main/java/ui/anwesome/com/linkedcircletorrview/LinkedCircleToRRView.kt
+++ linkedcircletorrview/src/main/java/ui/anwesome/com/linkedcircletorrview/LinkedCircleToRRView.kt
@@ -106,4 +106,47 @@ class LinkedCircleToRRView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CRRNode(var i : Int, val state : State = State()) {
+        private var next : CRRNode? = null
+        private var prev : CRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCRRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CRRNode {
+            var curr : CRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
