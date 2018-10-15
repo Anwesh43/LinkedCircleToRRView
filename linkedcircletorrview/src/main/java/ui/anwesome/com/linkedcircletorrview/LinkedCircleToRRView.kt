79e5f575ac184088844b064933b4051263e4519a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-15 22:19:10 +0530

    importing libraries and defining nodes

--- linkedcircletorrview/src/main/java/ui/anwesome/com/linkedcircletorrview/LinkedCircleToRRView.kt
+++ linkedcircletorrview/src/main/java/ui/anwesome/com/linkedcircletorrview/LinkedCircleToRRView.kt
@@ -3,3 +3,14 @@ package ui.anwesome.com.linkedcircletorrview
 /**
  * Created by anweshmishra on 15/10/18.
  */
+
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
